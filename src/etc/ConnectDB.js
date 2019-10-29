bd2d954a5b5d2c18f1737ed29f06f22e0b96b8b1
riann01 <rianinacio@gmail.com>
2019-10-29 14:17:48 -0300

    Alterações feitas na tela inicial

--- src/etc/ConnectDB.js
+++ src/etc/ConnectDB.js
@@ -1,17 +1,23 @@
-const { Client } = require('pg')
-import { Alert } from 'react-native'
+import Pool from 'pg';
+import { Alert } from 'react-native';
 
-const client = new Client({
-    user: "postgres",
-    password: "postgres",
-    host: "localhost",
+const client = new Pool({
+    user: 'postgres',
+    password: 'postgres',
+    host: 'localhost',
     port: 5432,
-    database: "IFG-Ambiental"
+    database: 'IFG-Ambiental'
 })
 
-function userLogin() {
-    client.connect()
-    .then(() => Alert.alert('Conexão realizada com sucesso.'))
-    .catch(() => Alert.alert('Hmm... Parece que você está sem conexão.'))
-    .finally(client.end())
+async function userLogin() {
+    try {
+        var response = await client.query("SELECT * FROM user;")
+        Alert.alert(response.rows)
+    }
+    catch(error) {
+        Alert.alert('Hmm... Parece que você está sem conexão. Erro: ' + error)
+    }
+    finally {
+        client.end()
+    }
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { StyleSheet, ImageBackground, Image } from 'react-native';
-import { mapping, dark as darkTheme } from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text, Button } from 'react-native-ui-kitten';
-const Client = require('../etc/ConnectDB');
+import { mapping, light as darkTheme } from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text, Button, IconRegistry, Icon } from 'react-native-ui-kitten';
+//const Client = require('../etc/ConnectDB');
 
-client = new Client();
+//client = new Client();
+
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+
+const FaceIcon = () => (
+  <Icon name='facebook' fill='white'/>
+);
+const GoogleIcon = (style) => (
+  <Icon {...style} name='google'/>
+);
+const EmailIcon = (style) => (
+  <Icon {...style} name='email'/>
+);
+const HostIcon = (style) => (
+  <Icon {...style} name='briefcase' style={{}}/>
+);
 
 const ApplicationContent = ({ navigation }) => (
   <React.Fragment>
@@ -12,42 +27,82 @@ const ApplicationContent = ({ navigation }) => (
     source={require('../../img/IF-background.png')}
     style={styles.imageBackground}>
     <Layout style={styles.container} level='1'>
-      <Image source={require('../../img/logo-ifg-letras-brancas.png')}
+      <Image source={require('../../img/logo-ifg.png')}
       style={{
         width: '80%',
         height: '20%',
         marginTop: 0,
       }}
       resizeMode="contain"/>
-      <Text style={styles.text} category='h4'>Bem Vindo ao IFG Ambiental</Text>
-      <Button onPress={() => client.userLogin()}
-      style={{marginTop: '30%'}}>
-      Quero postar logo!</Button>
+      <Text style={styles.text} category='h4'>Bem-vindo ao IFG Ambiental</Text>
+      <Button
+        style={{
+          marginTop: '10%',
+          width: '70%',
+        }}
+        icon={FaceIcon}>
+        Entrar com Facebook
+      </Button>
+      <Button
+        style={{
+          marginTop: '5%',
+          width: '70%'
+        }}
+        icon={GoogleIcon}
+        status='danger'>
+        Entrar com Google
+      </Button>
+      <Button
+        style={{
+          marginTop: '5%',
+          width: '70%'
+        }}
+        icon={EmailIcon}
+        status='success'>
+        Entrar com email
+      </Button>
+      <Button
+        style={{
+          marginTop: '5%',
+          marginBottom: '5%',
+          width: '70%'
+        }}
+        icon={HostIcon}
+        status='basic'
+        onPress={() => navigation.navigate('MainScreen')}>
+        Entrar como convidado 
+      </Button>
     </Layout>
     </ImageBackground>
   </React.Fragment>
 ); 
 
 const HomeScreen = () => (
+  <React.Fragment>
+    <IconRegistry icons={EvaIconsPack}/>
     <ApplicationProvider
     mapping={mapping}
     theme={darkTheme}>
         <ApplicationContent/>
     </ApplicationProvider>
+  </React.Fragment>
 );
-
 
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     textAlign:'center',
-    height: '60%',
+    height: '53%',
     width: '80%',
-    top: '25%',
+    top: '23%',
     textAlign: 'center',
     borderRadius: 10,
   },
-  text: { marginVertical: 0, textAlign: 'center' },
+  text: {
+    marginTop: 10,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
   imageBackground: {
     alignItems: 'center',
     width: '100%',
@@ -60,5 +115,6 @@ HomeScreen.navigationOptions = ({ /*navigation*/ }) => {
       header: null
   }
 }
+
 
 export default HomeScreen;
